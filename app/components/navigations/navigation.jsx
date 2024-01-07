63c1a8687dbd2df5a86acca02726e0af7b35a693
levanydze <66974840+levanydze@users.noreply.github.com>
2024-01-07 16:34:16 +0100

    navigation styles

--- app/components/navigations/navigation.jsx
+++ app/components/navigations/navigation.jsx
@@ -1,17 +1,46 @@
+"use client";
 import Link from "next/link";
 import React from "react";
 
 // import styles
 import styles from "./navigation.module.css";
+import { usePathname } from "next/navigation";
 
 export default function Navigation() {
+  const pathname = usePathname();
   return (
     <div className={styles.wrapper}>
       <ul className={styles.menuWrapper}>
-        <Link href={"/"}>HOME</Link>
-        <Link href={"/projects"}>PROJECTS</Link>
-        <Link href={"/photography"}>PHOTOGRAPHY</Link>
-        <Link href={"/contact"}>CONTACT</Link>
+        <Link
+          className={`${styles.link} ${pathname === "/" ? styles.active : ""}`}
+          href={"/"}
+        >
+          HOME
+        </Link>
+        <Link
+          className={`${styles.link} ${
+            pathname.startsWith("/projects") ? styles.active : ""
+          }`}
+          href={"/projects"}
+        >
+          PROJECTS
+        </Link>
+        <Link
+          className={`${styles.link} ${
+            pathname === "/photography" ? styles.active : ""
+          }`}
+          href={"/photography"}
+        >
+          PHOTOGRAPHY
+        </Link>
+        <Link
+          className={`${styles.link} ${
+            pathname === "/contact" ? styles.active : ""
+          }`}
+          href={"/contact"}
+        >
+          CONTACT
+        </Link>
       </ul>
     </div>
   );
