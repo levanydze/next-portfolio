2b7d4c09580a5e1c8d2909f7396b34a93e5a11be
levanydze <66974840+levanydze@users.noreply.github.com>
2024-01-07 12:44:01 +0100

    12

--- next.config.js
+++ next.config.js
@@ -1,25 +1,12 @@
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+};
+
+module.exports = nextConfig;
+
 module.exports = {
   images: {
     domains: ["github.com"],
   },
-  // Add the following lines for export configuration
-  target: "experimental-serverless-trace",
-  exportPathMap: async function () {
-    return {
-      "/": { page: "/" }, // You can add other pages as needed
-      // Example: "/projects/1": { page: "/projects/[projectId]", query: { projectId: 1 } },
-    };
-  },
 };
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// module.exports = nextConfig;
-
-// module.exports = {
-//   images: {
-//     domains: ["github.com"],
-//   },
-// };
