2c9706d5cab4222e518e1db2ebf266e916bbb544
levanydze <66974840+levanydze@users.noreply.github.com>
2024-02-25 17:59:31 +0100

    12

--- app/components/contactForm.jsx
+++ app/components/contactForm.jsx
@@ -5,68 +5,47 @@ import emailjs from "@emailjs/browser";
 //import styles
 import styles from "./contactForm.module.css";
 
-export default function ContactForm() {
+export const ContactForm = () => {
   const form = useRef();
-  const [emailSent, setEmailSent] = useState(false);
-  const HandleEmailSent = () => {
-    setEmailSent(true);
-  };
+
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_u6gjg6s",
-        "template_jsv5qfx",
-        form.current,
-        "y8nREYl_1OCsl1rBw"
-      )
-      .then(() => {
-        HandleEmailSent();
-      });
+    emailjs.sendForm(
+      "service_u6gjg6s",
+      "template_jsv5qfx",
+      form.current,
+      "y8nREYl_1OCsl1rBw"
+    );
   };
 
   return (
     <>
-      <div className={styles.formHead}>
-        <h2>Contact Us</h2>
-        <h3>LET'S GET IN TOUCH</h3>
-        <p className="twoLines"></p>
-      </div>
-      <form
-        className={`${styles.form} ${emailSent === true ? styles.none : ""}`}
-        ref={form}
-        onSubmit={sendEmail}
-      >
-        <div className={styles.smallInputs}>
+      <h2 className="headText">Contact</h2>
+      <div className={styles.wrapper}>
+        <form className={styles.form} ref={form} onSubmit={sendEmail}>
           <input
-            className={`${styles.input} ${styles.miniInput}`}
+            className={styles.input}
             type="text"
             name="user_name"
-            placeholder="Enter your full name"
+            placeholder="Name"
           />
           <input
-            className={`${styles.input} ${styles.miniInput}`}
+            className={styles.input}
             type="email"
             name="user_email"
             placeholder="Email"
           />
-        </div>
-
-        <textarea
-          className={`${styles.textArea} ${styles.input}`}
-          name="message"
-          placeholder="Your message here"
-        />
-        <input
-          className={`button1 ${styles.buttonSend}`}
-          type="submit"
-          value="Send"
-        />
-      </form>
-      <div className={emailSent ? styles.formSent : styles.hide}>
-        <h6> Thank you for your message</h6>
+          <textarea
+            className={`${styles.textArea} ${styles.input}`}
+            name="message"
+            placeholder="Your message here"
+          />
+          <input className={styles.button} type="submit" value="Send" />
+        </form>
       </div>
     </>
   );
-}
+};
+
+export default ContactForm;
