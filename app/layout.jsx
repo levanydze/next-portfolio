96b8ffc7e3022edab203113ecef1cb6496432db0
levanydze <66974840+levanydze@users.noreply.github.com>
2024-03-22 23:04:10 +0100

    1

--- app/layout.jsx
+++ app/layout.jsx
@@ -17,7 +17,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={openSans.className}>
+      <body className={inter.className}>
         <div className="myContainer">
           <Header />
           {children}
--- app/projects/card.jsx
+++ app/projects/card.jsx
@@ -14,30 +14,44 @@ export default function Card({
   important,
   filter,
   best,
+  github,
+  projectWeb,
 }) {
   return (
-    <div
-      className={`${styles.mainWrapper} ${filter ? styles.important : ""} ${
-        best && filter ? styles.best : ""
-      }`}
-    >
-      <Link className={styles.linkWrapper} href={`/projects/${id}`}></Link>
+    <div className={styles.mainWrapper}>
       <div className={styles.cardWrapper}>
-        <h3 className={styles.date}>{date}</h3>
+        <h3
+          className={`${styles.date} ${filter ? styles.important : ""} ${
+            best && filter ? styles.best : ""
+          }`}
+        >
+          {date}
+        </h3>
         <div className={styles.infoWrapper}></div>
         <div className={styles.photoDiv}>
-          <Image
-            width={500}
-            height={500}
-            src={image}
-            alt={description}
-            priority
-          ></Image>
+          <Link className={styles.linkWrapper} href={`/projects/${id}`}>
+            <Image
+              width={500}
+              height={500}
+              src={image}
+              alt={description}
+              priority
+            ></Image>
+          </Link>
         </div>
+      </div>
+      <div className={styles.liveGit}>
+        <Link href={projectWeb} className={styles.live}>
+          Wiev Live
+        </Link>
+        <Link href={`/projects/${id}`}>Wiev More</Link>
       </div>
       <div className={styles.titleDesc}>
         <div className={styles.title}>{title}</div>
-        <p> {description}</p>
+        <p>
+          {description.length > 50 ? description.substring(0, 50) : description}
+          ...
+        </p>
       </div>
     </div>
   );
